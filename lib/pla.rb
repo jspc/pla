fec319281fae10d50ea0048c6d4464966ea1d7c4
jspc <james@zero-internet.org.uk>
2016-05-29 13:38:55 +0100

    DRY up search and order mechanism

--- lib/pla.rb
+++ lib/pla.rb
@@ -18,44 +18,45 @@ class PLA
   def self.find movement_type, field, key
     if self.methods.include? movement_type
       movements = self.send(movement_type)
+      return self.munge_by_field movements, :select, field, key
+    end
+    []
+  end
 
-      if field =~ /^(.*)_(.*)$/
-        field = $1
-        field_key = $2
+  def self.sort movement_type, field
+    if self.methods.include? movement_type
+      movements = self.send(movement_type)
+      return self.munge_by_field movements, :sort_by, field
+    end
+    []
+  end
 
-        m = movements.select{|m| m[field.to_sym][field_key.to_sym] == key}
-      else
-        m = movements.select{|m| m[field.to_sym] == key}
-      end
-      m
+  def self.munge_by_field movements, method, field, search_value=nil
+    if field =~ /^(.*)_(.*)$/
+      field = $1
+      field_key = $2
+
+      search_value.nil? ?
+        movements.send(method){|m| m[field.to_sym][field_key.to_sym]} :
+        movements.send(method){|m| m[field.to_sym][field_key.to_sym] == search_value}
+
     else
-      []
+      search_value.nil? ?
+        movements.send(method){|m| m[field.to_sym]} :
+        movements.send(method){|m| m[field.to_sym] == search_value}
     end
   end
 
   def self.method_missing(m, *args, &block)
-    if m.to_s =~ /^find_(.*)_by_(.*)$/
-      movement_type = $1
-      field = $2
+    if m.to_s =~ /^(.*)_by_(.*)$/
+      movement_type = $1.to_sym
+      field = $2.to_sym
 
-      return self.find(movement_type.to_sym, field.to_sym, args.first)
-
-    elsif m.to_s =~ /^(.*)_by_(.*)$/
-      movement_type = $1
-      field = $2
-
-      if self.methods.include? movement_type.to_sym
-        movements = self.send(movement_type.to_sym)
-
-        if field =~ /^(.*)_(.*)$/
-          field = $1
-          field_key = $2
-
-          m = movements.sort_by{|m| m[field.to_sym][field_key.to_sym]}
-        else
-          m = movements.sort_by{|m| m[field.to_sym]}
-        end
-        return m
+      if movement_type.to_s =~ /^find_(.*)$/
+        movement_type = $1.to_sym
+        return self.find(movement_type, field, args.first)
+      else
+        return self.sort(movement_type, field)
       end
     end
     super
@@ -63,9 +64,7 @@ class PLA
 
   def self.respond_to?(method_sym, include_private = false)
     if method_sym.to_s =~ /^(.*)_by_(.*)$/
-      if self.methods.include? movement_type.to_sym
-        true
-      end
+      true
     else
       super
     end
